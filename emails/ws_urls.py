d64d2b911296fa7b16199d48aa741236bec86424
mitisit <29955395+mitisit89@users.noreply.github.com>
2024-11-05 08:16:32 +0300

    chore:rename

--- emails/ws_urls.py
+++ emails/ws_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from emails import consumer, views
 
 ws_urls = [
-    path("ws/emails/", costumer.EmailConsumer.as_asgi()),
+    path("ws/emails/", consumer.EmailConsumer.as_asgi()),
     path("emails/", views.email_list_view, name="email_list"),
     path("add-email-account/", views.add_email_account, name="add_email_account"),
 ]
